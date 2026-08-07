72c66a2ba0770073704d0676266229af70086065
arifhossain550665 <mdarifhossain550665@gmail.com>
2026-08-08 03:46:17 +0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/ipa_musicplayer_arif_ide/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ipa_musicplayer_arif_ide/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.ipa_musicplayer_arif_ide
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: AudioServiceActivity()
